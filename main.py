bc6dd9e273a652ab60c335957e1d80dba5aeaad0
Kyle Lucovsky <kyle@lucovsky.com>
2012-08-08 21:27:18 -0700

    massive re-org WOO

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+          'url': url,
+          'url_linktext': url_linktext,
+          }
+
+        path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
